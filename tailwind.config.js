8cbb89ca5a356945e1f89df929290d28597d3509
Mahdiyeh <zafarimahdiyeh@gmail.com>
2024-07-22 19:55:10 +0330

    changeConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,49 +17,60 @@ module.exports = {
           // background: linear-gradient(90deg, rgba(130,18,41,1) 35%, rgba(37,3,10,1) 100%);
           // background: linear-gradient(90deg, rgba(147,183,74,1) 0%, rgba(73,91,38,1) 50%, rgba(11,13,7,1) 100%);
       },
-      color:
+      container:
+      {
+        center:true
+      },
+      colors:
       {
         Primary:
         {
           
-          "100":"#EF4535",
-          "200":"#FF6A00",
+          100:'#EF4535',
+          200:"#FF6A00",
 
         },
         Secendry:
         {
-          "100":"#e0e0e0",
-          "200":"#9FA09F",
-          "300":"#696B69",
-          "400":"#3A3A3A"
+          100:'#e0e0e0',
+          200:'#9FA09F',
+          300:'#696B69',
+          400:'#3A3A3A'
         },
         Green:
         { 
-          "100":"#00BF6F",
-          "200":"#4CAF50",
-          "300":"#1D9A34"
+          1000:'#00BF6F',
+          1100:'#4CAF50',
+          1200:'#1D9A34'
         },
         Red:
         {
-          "100":"#F41E1E",
-          "200":"#D32F2F"
+          1000:'#F41E1E',
+          1100:'#D32F2F'
         },
         Sorrel:
         {
-          "100":"#272C48",
-          "200":"#272C48",
-          "300":"#1B1F35",
+          100:'#272C48',
+          200:'#272C48',
+          300:'#1B1F35',
         },
-        "Gold":"#F7A468",
-        "Purple":"#5947DA",
-        "Bronze":"#BA740B",
-        "Dark Blue":"#0B3193",
-        "Blue":"#1061EF",
-        "Ciyan":"#0FABC6",
+        "parsGold":"#F7A468",
+        "parsPurple":"#5947DA",
+        "parsBronze":"#BA740B",
+        "parsDarkBlue":"#0B3193",
+        "parsBlue":"#1061EF",
+        "parsCyan":"#0FABC6",
         
         
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function({addVariant})
+    {
+      addVariant('child','& > *');
+      addVariant('child-hover','& > *:hover');
+
+    }
+  ],
 };
