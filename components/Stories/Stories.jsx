942c2f6c3553c6186af6887bcb24ef1ad8cd9bf8
ftmhabt <ftmh.a78@gmail.com>
2024-07-23 15:59:10 +0330

    Hide the scrollbar

--- components/Stories/Stories.jsx
+++ components/Stories/Stories.jsx
@@ -4,7 +4,7 @@ import data from "./data.json"
 export default function Stories() {
   
   return (
-    <div className="flex gap-5 p-5 overflow-x-scroll w-screen">
+    <div className="flex gap-5 p-5 overflow-x-scroll w-screen scrollbar-none">
       {data.map((story) => (
         <Story key={story.id} name={story.username} image={story.image}/>
       ))}
