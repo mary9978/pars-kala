adf0bf7e37c1377231711e7b6abfd4c2a72e4405
ftmhabt <ftmh.a78@gmail.com>
2024-08-25 11:59:08 +0330

    Add carousel

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,9 @@
-
-import React from 'react';
+import Carousel from "@/components/Carousel";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
-   
+    <>
+      <Carousel />
+    </>
   );
 }
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,59 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { useRef } from "react";
+
+export default function Carousel() {
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty("--progress", 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
+
+  return (
+    <div
+      id="container"
+      class="container bg-neutral-100 relative p-2 max-w-[1280px] w-full flex justify-center mt-5 lg:mt-20"
+    >
+      <Swiper
+        modules={[Autoplay, Pagination, Navigation]}
+        spaceBetween={30}
+        slidesPerView={1}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{ clickable: true }}
+        navigation
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+      >
+        <SwiperSlide>
+          <img src="./img/head1.jpg" class="w-full h-full" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./img/head2.jpg" class="w-full h-full" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./img/head3.jpg" class="w-full h-full" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./img/head4.jpg" class="w-full h-full" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./img/head5.jpg" class="w-full h-full" alt="" />
+        </SwiperSlide>
+        <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
+        </div>
+      </Swiper>
+    </div>
+  );
+}
